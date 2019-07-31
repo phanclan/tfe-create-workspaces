82fcfec5584f6e7fd4c06a74c29b4cc83db96552
ppresto <ppresto@hashicorp.com>
2019-07-30 18:41:57 -0700

    test6

--- main.tf
+++ main.tf
@@ -25,8 +25,9 @@ resource "tfe_team" "teams" {
 }
 
 resource "tfe_team_access" "test" {
-  access       = "read"
-  team_id      = "${lookup(tfe_team.teams.id, count.index)}"
+  access = "read"
+  #team_id      = "${lookup(tfe_team.teams.id, count.index)}"
+  team_id      = "${tfe_team.teams.*.id[count.index]}"
   workspace_id = "${var.organization}/${element(var.workspace_ids, count.index)}"
 }
 
